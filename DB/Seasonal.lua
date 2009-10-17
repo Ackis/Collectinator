4bc9ba7d19527f2352c29b0ee85385ed77810227
James D. Callahan III <darkenelf@gmail.com>
2009-10-17 00:57:59 -0400

    In addon:InitSeasons(): Only grab the first return value of GetCategoryInfo().

--- DB/Seasonal.lua
+++ DB/Seasonal.lua
@@ -28,13 +28,13 @@ local addon	= LibStub("AceAddon-3.0"):GetAddon(MODNAME)
 local L		= LibStub("AceLocale-3.0"):GetLocale(MODNAME)
 
 function addon:InitSeasons(DB)
-	self:addLookupList(DB, 1, GetCategoryInfo(156))	-- Winter Veil
-	self:addLookupList(DB, 2, GetCategoryInfo(160))	-- Lunar Festival
+	self:addLookupList(DB, 1, select(1, GetCategoryInfo(156)))	-- Winter Veil
+	self:addLookupList(DB, 2, select(1, GetCategoryInfo(160)))	-- Lunar Festival
 	self:addLookupList(DB, 3, L["Darkmoon Faire"])
-	self:addLookupList(DB, 4, GetCategoryInfo(161))	-- Midsummer
-	self:addLookupList(DB, 5, GetCategoryInfo(187)) -- Love is in the air
-	self:addLookupList(DB, 6, GetCategoryInfo(162))	-- Brewfest
-	self:addLookupList(DB, 7, GetCategoryInfo(158))	-- Hallow's End
-	self:addLookupList(DB, 8, GetCategoryInfo(163))	-- Children's Week
-	self:addLookupList(DB, 9, GetCategoryInfo(159))	-- Noblegarden
+	self:addLookupList(DB, 4, select(1, GetCategoryInfo(161)))	-- Midsummer
+	self:addLookupList(DB, 5, select(1, GetCategoryInfo(187)))	-- Love is in the air
+	self:addLookupList(DB, 6, select(1, GetCategoryInfo(162)))	-- Brewfest
+	self:addLookupList(DB, 7, select(1, GetCategoryInfo(158)))	-- Hallow's End
+	self:addLookupList(DB, 8, select(1, GetCategoryInfo(163)))	-- Children's Week
+	self:addLookupList(DB, 9, select(1, GetCategoryInfo(159)))	-- Noblegarden
 end
