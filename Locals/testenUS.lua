355da4c3cf3c45d7c3216d25d382999d0da6a96c
John Pasula <ackis@shaw.ca>
2009-10-19 10:24:33 -0600

    Typo fix.

--- Locals/testenUS.lua
+++ Locals/testenUS.lua
@@ -40,7 +40,7 @@ L["CLEAR_EXCLUSION_LIST_DESC"] = "Removes all entries from the exclusion list."
 L["Close GUI"] = true
 L["CLOSE_DESC"] = "Close the Collectinator Window."
 --L["CLOSEGUI_DESC"] remove ref
-L["COLLECTIBLE_EXCLUDED"] = 
+--L["COLLECTIBLE_EXCLUDED"]
 L["Collectinator_ALLEXCLUDED"] = "Your exclusions are currently preventing any entries from being displayed. Please change your exclusions and try again."
 L["Collectinator_ALLKNOWN"] = "You know all the entries, there is nothing left for you to collect."
 L["Collectinator_SEARCHFILTERED"] = "Your search has no results."
