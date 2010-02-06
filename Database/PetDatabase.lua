740e8254d9c3f48361e02af2dfab3cb211f7e55e
pompy <Mike@.(none)>
2010-02-06 13:56:16 -0500

    Removed an incorrect flag from "Cat Carrier (Black Tabby)"

--- Database/PetDatabase.lua
+++ Database/PetDatabase.lua
@@ -276,7 +276,7 @@ function addon:GetMiniPetTotal(PetDB)
 
 	-- Cat Carrier (Black Tabby) - 10675
 	AddPet(10675, 8491, R_COMMON, GAME_ORIG)
-	self:AddCompanionFlags(PetDB, 10675, F_QUEST, F_MOB_DROP, F_BOE)
+	self:AddCompanionFlags(PetDB, 10675, F_MOB_DROP, F_BOE)
 	self:AddCompanionAcquire(PetDB, 10675, A_MOB, 1920, A_MOB, 2271, A_MOB, 2272, A_MOB, 2358)
 
 	-- Cat Carrier (Siamese) - 10677
