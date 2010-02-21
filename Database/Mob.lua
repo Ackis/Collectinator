ae45abb33a3dbdb80adce0428552ec14ffbdd4a3
pompy <Mike@.(none)>
2010-02-21 13:20:21 -0500

    Add "Toravon the Ice Watcher" to the Mob database.

--- Database/Mob.lua
+++ Database/Mob.lua
@@ -94,5 +94,6 @@ function addon:InitMob(DB)
 	self:addLookupList(DB, 34564,	BB["Anub'arak"],			BZ["Trial of the Crusader"],		0, 0)
 	self:addLookupList(DB, 35013,	BB["Koralon the Flame Watcher"],	BZ["Vault of Archavon"],		0, 0)
 	self:addLookupList(DB, 36296,	L["Apothecary Hummel"],	BZ["Shadowfang Keep"],		0, 0)
+	self:addLookupList(DB, 38433, BB["Toravon the Ice Watcher"],	BZ["Vault of Archavon"],		0, 0)
 	
 end
