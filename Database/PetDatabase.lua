fc75ecd65b0bfa1620f26ce849388ade9fb58bc6
mike <mike@broinitup.com>
2011-07-31 00:44:20 -0400

    Updated the "Scorched Stone" entry

--- Database/PetDatabase.lua
+++ Database/PetDatabase.lua
@@ -264,8 +264,8 @@ function addon:GetMiniPetTotal(PetDB)
 
 	-- Scorched Stone - 45890
 	AddPet(45890, 34955, R_RARE, GAME_BC)
-	self:AddCompanionFlags(PetDB, 45890, F_ALLIANCE, F_HORDE, F_INSTANCE, F_SEASONAL, F_MOB_DROP, F_BOP, F_REMOVED)
-	self:AddCompanionAcquire(PetDB, 45890, A_MOB, 25740, A_SEASONAL, 4)
+	self:AddCompanionFlags(PetDB, 45890, F_ALLIANCE, F_HORDE, F_INSTANCE, F_SEASONAL, F_MOB_DROP, F_VENDOR, F_BOP)
+	self:AddCompanionAcquire(PetDB, 45890, A_MOB, 25740, A_SEASONAL, 4, A_VENDOR, 52822, A_CUSTOM, 15)
 
 	-- Clockwork Rocket Bot - 54187
 	AddPet(54187, 34425, R_RARE, GAME_BC)
