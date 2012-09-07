8a8f35e552f90385efe5122d95838c2b174ff585
John Pasula <ackis@shaw.ca>
2012-09-07 12:42:45 -0600

    Clean up localizations (point to proper place, nuke all in dev

---
+++ Locales/devel-enUS.lua
@@ -0,0 +1,21 @@
+--[[
+************************************************************************
+devel-enUS.lua
+These are localization strings used for the testing of collectinator.
+Manually add entries here and then proceed to update the localization
+application located here:
+	http://www.wowace.com/addons/collectinator/localization/ 
+************************************************************************
+File date: @file-date-iso@
+File revision: @file-revision@
+Project revision: @project-revision@
+Project version: @project-version@
+************************************************************************
+Please see http://www.wowace.com/addons/collectinator/ for more information.
+These translations are released under the Public Domain.
+************************************************************************
+]]--
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Collectinator", "enUS", true)
+
+if not L then return end
