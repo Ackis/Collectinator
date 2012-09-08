464867837d0ad6776f4a7371a33bf16e6daa9a59
John Pasula <ackis@shaw.ca>
2012-09-08 12:03:47 -0600

    Add a few entries to the database for testing.

---
+++ Database/PetDB.lua
@@ -0,0 +1,56 @@
+﻿--[[
+************************************************************************
+MountDatabase.lua
+************************************************************************
+File date: @file-date-iso@
+File hash: @file-abbreviated-hash@
+Project hash: @project-abbreviated-hash@
+Project version: @project-version@
+************************************************************************
+Please see http://www.wowace.com/addons/collectinator/ for more information.
+************************************************************************
+This source code is released under All Rights Reserved.
+************************************************************************
+]] --
+
+-------------------------------------------------------------------------------
+-- Localized Lua globals.
+-------------------------------------------------------------------------------
+local _G = getfenv(0)
+
+-------------------------------------------------------------------------------
+-- AddOn namespace.
+-------------------------------------------------------------------------------
+local FOLDER_NAME, private = ...
+
+local LibStub	= _G.LibStub
+local addon = LibStub("AceAddon-3.0"):GetAddon(private.addon_name)
+local L = LibStub("AceLocale-3.0"):GetLocale(private.addon_name)
+
+-------------------------------------------------------------------------------
+-- Filter flags. Acquire types, and Reputation levels.
+-------------------------------------------------------------------------------
+local F = private.FILTER_IDS
+local A = private.ACQUIRE_TYPES
+local Q = private.ITEM_QUALITIES
+local REP = private.REP_LEVELS
+local FAC = private.FACTION_IDS
+local V = private.GAME_VERSIONS
+local Z = private.ZONE_NAMES
+
+--------------------------------------------------------------------------------------------------------------------
+-- Initialize!
+--------------------------------------------------------------------------------------------------------------------
+
+function addon:InitMounts()
+
+	local function AddCol(spell_id, genesis, quality)
+		return addon:AddCollection(spell_id, "MOUNT", genesis, quality)
+	end
+
+	local collection
+
+	-- Zergling - 11327
+	collection = AddCol(11327, V.ORIG, Q.COMMON)
+
+end
