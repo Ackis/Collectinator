34856173371f3c4b29e652953a152d1617844b9a
Ressy <resa1983@hotmail.com>
2012-10-11 01:19:04 -0400

    Add Quests which give Companions/mounts

--- Database/Quest.lua
+++ Database/Quest.lua
@@ -54,7 +54,24 @@ function addon:InitQuest()
 		private:AddListEntry(private.quest_list, quest_id, nil, zone_name, coord_x, coord_y, faction)
 	end
 
-	AddQuest(29412,	Z.STORMWIND_CITY,		67.2,	85.5,	"Alliance") -- needs coords updated
+	AddQuest(3721,	Z.THE_CAPE_OF_STRANGLETHORN,	43.0,	72.0,	"Neutral")
+	AddQuest(3861,	Z.WESTFALL,			56.0,	31.6,	"Neutral")
+	AddQuest(7675,	Z.DUN_MOROGH,			56.2,	46.2,	"Alliance")
+	AddQuest(7676,	Z.DUN_MOROGH,			56.2,	46.2,	"Alliance")
+	AddQuest(10898,	Z.TEROKKAR_FOREST,		56.6,	72.8,	"Neutral")
+	AddQuest(13570,	Z.DARKSHORE,			50.8,	18.0,	"Alliance")
+	AddQuest(13702,	Z.ICECROWN,			76.6,	19.2,	"Alliance") -- Quest IDs 13702, 13732-13735
+	AddQuest(13736,	Z.ICECROWN,			76.6,	24.4,	"Horde") -- Quest IDs 13736-13740
+	AddQuest(25560,	Z.MOUNT_HYJAL,			39.1,	58.3,	"Neutral")
+	AddQuest(28415, Z.BURNING_STEPPES,		71.8,	68.0,	"Alliance")
+	AddQuest(28748,	Z.HILLSBRAD_FOOTHILLS,		33.6,	49.2,	"Neutral")
+	AddQuest(29034,	Z.WINTERSPRING,			46.6,	17.6,	"Alliance")
+	AddQuest(29208,	Z.NORTHERN_STRANGLETHORN,	0.0,	0.0,	"Neutral")
+	AddQuest(29267, Z.NORTHERN_STRANGLETHORN,	76.0,	66.6,	"Alliance")
+	AddQuest(29268, Z.NORTHERN_STRANGLETHORN,	76.0,	66.6,	"Horde")
+	AddQuest(29401, Z.ORGRIMMAR,			48.0,	47.0,	"Horde")
+	AddQuest(29412,	Z.STORMWIND_CITY,		58.8,	52.8,	"Alliance")
+	AddQuest(29905,	Z.THE_JADE_FOREST,		58.9,	81.7,	"Alliance")
 
 	self.InitQuest = nil
 end
