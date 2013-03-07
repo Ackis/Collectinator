3e296272c4f937236390ad76b6ade5967960c5cd
John Pasula <ackis@shaw.ca>
2013-03-06 22:36:04 -0700

    DALARN -> DALARAN

--- Database/Quest.lua
+++ Database/Quest.lua
@@ -81,7 +81,7 @@ function addon:InitQuest()
 	AddQuest(13702,	Z.ICECROWN,				76.6,	19.2,	"Alliance") -- Quest IDs 13702, 13732-13735
 	AddQuest(13736,	Z.ICECROWN,				76.6,	24.4,	"Horde") -- Quest IDs 13736-13740
 	AddQuest(13906,	Z.UNGORO_CRATER,			71.4,	73.8,	"Horde")
-	AddQuest(13959,	Z.DALARN,				 0.0,	 0.0,	"Neutral")
+	AddQuest(13959,	Z.DALARAN,				 0.0,	 0.0,	"Neutral")
 	AddQuest(24915,	Z.ICECROWN_CITADEL,			 0.0,	 0.0,	"Neutral")
 	AddQuest(25371,	Z.KELPTHAR_FOREST,			46.0,	46.8,	"Neutral")
 	AddQuest(25560,	Z.MOUNT_HYJAL,				39.1,	58.3,	"Neutral")
