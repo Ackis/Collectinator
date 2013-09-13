f34c864d5ecbe9d98f3d048d6aae9e163e8f8c77
Teresa <resa1983@hotmail.com>
2013-09-13 11:56:19 -0400

    Copying ARL fix here

--- Core.lua
+++ Core.lua
@@ -439,7 +439,7 @@ function addon:OnInitialize()
 		end
 		local name, tooltip_unit = self:GetUnit()
 
-		if not tooltip_unit then
+		if not tooltip_unit or not _G.UnitGUID(tooltip_unit) then
 			return
 		end
 		local id_num = private.MobGUIDToIDNum(_G.UnitGUID(tooltip_unit))
